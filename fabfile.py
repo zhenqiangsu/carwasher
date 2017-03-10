496c71c29156afc44d5f31e0347238bc1c49b9d5
zhenqiangsu <zhenqiangsu@gmail.com>
2017-03-10 08:57:28 -0500

    add fabfile.py file

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import env, run, cd
+
+USERNAME = 'root'
+SERVER = 'zsu.mydomain.com'
+APP_NAME = 'carwasher'
+PROJECT_DIR = '/var/www/wsgi_apps/%s' % (APP_NAME)
+WSGI_SCRIPT = '../carwasher.wsgi'
+
+env.hosts = ["%s@%s" % (USERNAME, SERVER)]
+
+def deploy():
+    with cd(PROJECT_DIR):
+        run('git pull')
+        run('source venv/bin/activate; pip install -r requirements.txt')
+        run('touch %s' % WSGI_SCRIPT)
